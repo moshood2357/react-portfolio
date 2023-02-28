76b680cd4fdfb701e3d65c5e2f914f21b47a5b44
Moshood Sanusi <sanusimoshoodabiola@gmail.com>
2023-02-28 20:17:44 +0100

    update on linking to whatsapp

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
           </div>
 
           <div>
-            <a href = "/">
+            <a href="https://chatwith.io/s/moshood-sanusi">
               <button className = "group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">WhatsApp</button>
             </a>
           </div>
